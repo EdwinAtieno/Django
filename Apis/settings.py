643eb671fcc23b5a8e1b870bd222a5a391d9b496
eddym <edwinmuthee11@gmail.com>
2022-03-05 22:12:17 +0300

    login app

--- Apis/settings.py
+++ Apis/settings.py
@@ -11,13 +11,14 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-3y)7rzclzx&5-_6i-@+id83feft=x8h%_g&4#54mk$ani^&wrh'
